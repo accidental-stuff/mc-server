91e96e3d1cbd6a7f897458b53d14f5f4323bffe7
mishalshanavas <mishalshanavas8@gmail.com>
2026-06-01 15:38:29 +0530

    Update : project id

--- providers.tf
+++ providers.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("~/.config/gcp/terraform.json")
+  credentials = file(pathexpand("~/.config/gcp/terraform.json"))
 
   project = var.project_id
   region  = var.region
